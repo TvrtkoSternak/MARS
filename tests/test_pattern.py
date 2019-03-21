e4fef964a9547d78d018ac43e20906669150d88f
tomi77 <tomislav-ante.brncic@fer.hr>
2019-03-21 15:05:51 +0100

    Add test for EditScript iterator

---
+++ tests/test_pattern.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+from unittest import main
+import ast
+import mars.pattern
+
+
+class TestEditScript(TestCase):
+    def test_iterating_edit_script(self):
+        """
+        Test that checks iterating through change operations.
+        """
+        insert = mars.pattern.Insert(1, ast.Add)
+        changes = [insert]
+        edit_script = mars.pattern.EditScript(changes)
+        it = iter(edit_script)
+        self.assertEqual(next(it), insert)
+        with self.assertRaises(StopIteration):
+            next(it)
+        i = 0
+        for e in edit_script:
+            i = i + 1
+        self.assertEqual(i, 1)
+
+
+if __name__ == 'main':
+    main()

ecb9857b2245994c7217111da383dc86fadf72aa
tomi77 <tomislav-ante.brncic@fer.hr>
2019-04-26 14:32:39 +0200

    File upload

--- web/web/urls.py
+++ web/web/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('upload', views.index, name='upload'),
     path('admin/', admin.site.urls),
 ]
--- web/web/views.py
+++ web/web/views.py
@@ -5,3 +5,13 @@ from django.template import loader
 def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render(None,request))
+
+def upload(request):
+    id = request.POST['id']
+    path = '/var/www/pictures/%s' % id
+    f = request.FILES['picture']
+    destination = open(path, 'wb+')
+    for chunk in f.chunks():
+        destination.write(chunk)
+    destination.close()
+    # return HttpResponse(open(path).read())

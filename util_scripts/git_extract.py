0b808a750f00e9f77ad46af4dbea9d5659148b91
tomi77 <tomislav-ante.brncic@fer.hr>
2019-05-02 16:20:38 +0200

    Extract ast body

--- util_scripts/git_extract.py
+++ util_scripts/git_extract.py
@@ -1,12 +1,13 @@
+import ast
+
+import astunparse
 import git
 
 repo = git.Repo("D:/DIPLOMSKI RAD/test")
 commits_list = list(repo.iter_commits('pattern_generalisation'))
 
-print(len(commits_list))
-
-a_commit = commits_list[1]
-b_commit = commits_list[0]
+a_commit = commits_list[2]
+b_commit = commits_list[1]
 
 diff_index = a_commit.diff(b_commit)
 print(diff_index)
@@ -14,3 +15,5 @@ for diff_item in diff_index.iter_change_type('M'):
     print(diff_item)
     a_blob = diff_item.a_blob.data_stream.read().decode('utf-8')
     b_blob = diff_item.b_blob.data_stream.read().decode('utf-8')
+    print(astunparse.unparse(ast.parse(b_blob).body[1].body[1]))
+    print(ast.parse(b_blob).body[1].body[1].body[0].value)

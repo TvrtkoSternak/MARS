01d14035af1589d4162bd1b02cfbf008fe582f56
tomi77 <tomislav-ante.brncic@fer.hr>
2019-05-02 16:27:57 +0200

    Add iteration through all commits

--- util_scripts/git_extract.py
+++ util_scripts/git_extract.py
@@ -3,17 +3,21 @@ import ast
 import astunparse
 import git
 
+
+def compare_commits(a_commit, b_commit):
+    diff_index = a_commit.diff(b_commit)
+    print(diff_index)
+    print(ast.parse("string").body[0].value)
+    for diff_item in diff_index.iter_change_type('M'):
+        print(diff_item)
+        a_blob = diff_item.a_blob.data_stream.read().decode('utf-8')
+        b_blob = diff_item.b_blob.data_stream.read().decode('utf-8')
+        #print(astunparse.unparse(ast.parse(b_blob).body[1].body[1]))
+        #print(ast.parse(b_blob).body[1].body[1].body[0].value)
+
+
 repo = git.Repo("D:/DIPLOMSKI RAD/test")
 commits_list = list(repo.iter_commits('pattern_generalisation'))
 
-a_commit = commits_list[2]
-b_commit = commits_list[1]
-
-diff_index = a_commit.diff(b_commit)
-print(diff_index)
-for diff_item in diff_index.iter_change_type('M'):
-    print(diff_item)
-    a_blob = diff_item.a_blob.data_stream.read().decode('utf-8')
-    b_blob = diff_item.b_blob.data_stream.read().decode('utf-8')
-    print(astunparse.unparse(ast.parse(b_blob).body[1].body[1]))
-    print(ast.parse(b_blob).body[1].body[1].body[0].value)
+for i in range(len(commits_list)-1):
+    compare_commits(commits_list[i+1], commits_list[i])

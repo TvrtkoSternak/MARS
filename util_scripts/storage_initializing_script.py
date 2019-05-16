465da3370b86d942c1c5050e891369b72da79668
Tvrtko Sternak <tsternak@hotmail.com>
2019-05-16 15:26:07 +0200

    remove comented code

--- util_scripts/storage_initializing_script.py
+++ util_scripts/storage_initializing_script.py
@@ -8,12 +8,6 @@ parser = CounterPatternParser()
 script_generator = EditScriptGenerator(TreeDifferencer(2, 0, 0), 0)
 pattern_creator = PatternCreator(StorageContext(filename), parser, script_generator)
 
-# for i in range(1, 5):
-#     file_org = "../resources/pattern_"+i.__str__()+"_org.pat"
-#     file_mod = "../resources/pattern_"+i.__str__()+"_mod.pat"
-#     pattern = pattern_creator.create_pattern(file_org, file_mod)
-#     pattern_creator.save_pattern(pattern)
-
 for directory, children, files in os.walk("../dataset"):
     if directory.split('/')[-1] not in 'dataset':
         print(directory)

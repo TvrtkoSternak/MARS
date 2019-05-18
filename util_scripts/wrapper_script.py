9b781fd35400daef97ea07759d0df07904d2ab32
tomi77 <tomislav-ante.brncic@fer.hr>
2019-05-18 18:35:43 +0200

    Call editScriptGenerator

--- util_scripts/wrapper_script.py
+++ util_scripts/wrapper_script.py
@@ -2,6 +2,7 @@ import ast
 
 from mars.astutils import AstWrapper
 from mars.pattern_creation import TreeDifferencer
+from mars.pattern_creation_marst import EditScriptGenerator
 
 with open("../dataset/variable_check/original_2.py") as original, open("../dataset/variable_check/modified_2.py") as modified:
     original_code = ast.parse(original.read())
@@ -13,7 +14,11 @@ org = wrapper.visit(original_code)
 mod = wrapper.visit(modified_code)
 
 tree_diff = TreeDifferencer(0.1)
-tree_diff.connect_nodes(org, mod)
+# tree_diff.connect_nodes(org, mod)
+
+generator = EditScriptGenerator(tree_diff, 0.3)
+generator.generate(org, mod)
+
 # wrapper = AstWrapper()
 # body = wrapper.visit(original_code)
 #

07cc60295c5099205ef7eece79168199402e0740
tomi77 <tomislav-ante.brncic@fer.hr>
2019-05-22 02:27:04 +0200

    Add to dataset

--- dataset/true/modified_11.py
+++ dataset/true/modified_11.py
@@ -1,4 +1,4 @@
-if isinstance(count, int):
+if isinstance(no_of_nodes, int):
     say("ah ah ah")
     say(count, "is the number")
     say_again("the number is", count)
--- dataset/true/original_11.py
+++ dataset/true/original_11.py
@@ -1,4 +1,4 @@
-if type(count) is type(int):
+if type(no_of_nodes) is type(int):
     say("ah ah ah")
     say(count, "is the number")
     say_again("the number is", count)

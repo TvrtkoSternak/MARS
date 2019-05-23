f8e3e12f4abfe6dfd9a5b16da4b84c7f5f4c7d0f
tomi77 <tomislav-ante.brncic@fer.hr>
2019-05-23 17:52:57 +0200

    Fix tabulate

--- web/web/views.py
+++ web/web/views.py
@@ -15,7 +15,7 @@ def analyse_code(request):
     source_code_lines_list = list()
     recommendations_list = list()
     for line in source_code.split('\n'):
-        source_code_lines_list.append(line)
+        source_code_lines_list.append(line.replace("    ", "\t"))
     for line in recommendations.strip(' \t\n\r').split('\n'):
         recommendations_list.append(line+'\n')
     return render(request, 'index.html', {"source_code": source_code_lines_list, "recommendations": recommendations_list})
@@ -25,5 +25,5 @@ def upload_file(request):
     source_code_file = request.FILES["source_code"]
     source_code_lines_list = list()
     for line in source_code_file.read().decode("utf-8").split('\n'):
-        source_code_lines_list.append(line)
+        source_code_lines_list.append(line.replace("    ", "\t"))
     return render(request, 'index.html', {"source_code": source_code_lines_list})

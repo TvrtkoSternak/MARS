74b9991e258e7d6cb27dda10d96a1caf5681a1ee
tomi77 <tomislav-ante.brncic@fer.hr>
2019-05-23 15:20:30 +0200

    Add more examples to dataset

---
+++ dataset/true/modified_23.py
@@ -0,0 +1,3 @@
+if lot_numbers:
+    sum_num(lot_numbers)
+    something_with_numbers(lot_numbers)
---
+++ dataset/true/original_24.py
@@ -0,0 +1,3 @@
+if len(lots_of_numbers) != 0:
+    print_sum(lots_of_numbers)
+    do_something_with_numbers(lots_of_numbers)
